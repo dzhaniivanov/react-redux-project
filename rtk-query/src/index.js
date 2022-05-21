554e1c7ba2bb442c8058fa5e9f5b598f6d9666a3
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-05-21 16:18:11 +0300

    added todolist and apislice with rtk query

--- rtk-query/src/index.js
+++ rtk-query/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { apiSlice } from "./features/api/apiSlice";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
